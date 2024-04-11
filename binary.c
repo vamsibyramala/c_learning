9ddcc8df33dd9beb1ed5fd8fae4d1cb933fdc555
Vamsi <vamsibyramala@gmail.com>
2024-04-11 13:06:15 +0530

    binary modified

--- binary.c
+++ binary.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 void main ()
 {
-    int x=10, y;
-    y=x++;
+    int x=1, a, b, c;
+    a=++x;
+    b=x++;
+    c=--x;
     printf("x=%d \n", x);
-    printf("y=%d \n", y);
+    printf("a=%d \n", a);
+    printf("b=%d \n", ab;
+    printf("c=%d \n", c);
 }
