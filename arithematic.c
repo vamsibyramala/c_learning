a96ebed879f1e750bc5facccc738c82558423255
root <root@ip-172-31-9-194.ap-south-1.compute.internal>
2024-04-11 07:15:58 +0000

    arithematic

---
+++ arithematic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	int a=10, b=7;
+	printf("a+b=%d \n",a+b);
+	printf("a-b=%d \n",a-b);
+	printf("a*b=%d \n",a*b);
+	printf("a/b=%d \n", a/b);
+	printf("mod=%d \n", a%b);
+}
