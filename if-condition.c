3eb0ede9cafd888547f3844f1858a4a642a9da33
Vamsi <vamsibyramala@gmail.com>
2024-04-12 16:07:16 +0530

    if-condition added

---
+++ if-condition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a=");
+    scanf("&d \n", &a);
+    if (a)
+    {
+        printf("inside if block");
+    }
+    printf("outside if block");
+
+    
+}
