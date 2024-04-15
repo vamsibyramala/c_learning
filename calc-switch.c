88cde12357e64b1289f4d18e6b05622b8a7b04a0
Vamsi <vamsibyramala@gmail.com>
2024-04-15 17:52:37 +0530

    switch-calc

---
+++ calc-switch.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int a, b;
+    char o;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("enter the operator:");
+    scanf("%c",&o);
+    printf("Enter second number:");
+    scanf("%d", &b);
+    switch (o)
+    {
+    case '+':
+        printf("sum = %d\n", a+b);
+        break;
+    case '-':
+        printf("subtraction = %d \n", a-b);
+        break;
+    case '*':
+        printf("multiplication = %d \n", a*b);
+        break;
+    case '/':
+        printf("division = %d \n", a/b);
+        break;
+    
+    default:
+        break;
+    }
+}
