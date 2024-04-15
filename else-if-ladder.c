f8f851e253d99ea381bdc3821c032aa5db6f55be
root <root@ip-172-31-9-194.ap-south-1.compute.internal>
2024-04-15 11:40:07 +0000

    else-f out

--- else-if-ladder.c
+++ else-if-ladder.c
@@ -6,14 +6,14 @@ void main()
     scanf("%d", &a);
     if(a==0)
     {
-        printf("the number is nun");
+        printf("the number is nun\n");
     }
     else if(a%2==0)
     {
-        printf("the number is even");
+        printf("the number is even\n");
     }
     else
     {
-        printf("the number is odd");
+        printf("the number is odd\n");
     }
 }
