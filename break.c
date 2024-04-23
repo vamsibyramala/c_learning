1dad8d29ba85c28ac71afdaa1cd517dcb14db17b
Vamsi <vamsibyramala@gmail.com>
2024-04-23 16:52:28 +0530

    break

---
+++ break.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a, i, sum=0;
+    for (i = 0; i < = 5; i++)
+    {
+        printf("Enter %d number:", i);
+        scanf("%d", &a);
+        sum = sum + a;
+    }
+    printf("sum = %d", sum);
+    
+    
+}
