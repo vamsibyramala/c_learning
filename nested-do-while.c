ce626c82b36f1bd49ee66c3d0a32ee92acf4fd35
Vamsi <vamsibyramala@gmail.com>
2024-04-23 18:18:04 +0530

    nested-do-while

---
+++ nested-do-while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    do
+    {
+        do
+        {
+        j=1;
+        printf("*");
+        j++;
+        } while (j<=5);
+        printf("\n");
+        i++;
+    } while (i<=5);
+}
